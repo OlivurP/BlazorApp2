9c268e3aa3a90cd7f7ba70440bba87728c2c2a2c
OlivurP <olivur@live.com>
2023-09-27 15:09:49 +0200

    Add project files.

---
+++ BlazorApp2/Data/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp2.Data
+{
+    public class Person
+    {
+        public string? Active { get; set; }
+        public int Age { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Url { get; set; }
+    }
+}
